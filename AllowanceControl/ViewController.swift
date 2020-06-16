dbc8721f673e67990b29e197b2f6762e30eb7824
Helio Junior <helio.engenharia@gmail.com>
2020-06-16 19:52:47 -0300

    Initial Commit

---
+++ AllowanceControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllowanceControl
+//
+//  Created by Helio Junior on 16/06/20.
+//  Copyright © 2020 HelioTecnologia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
